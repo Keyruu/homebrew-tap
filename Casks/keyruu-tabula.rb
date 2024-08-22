1fefac795a32e9dbe69835ac6d1da449d0e747f1
Lucas <keyruu@web.de>
2024-08-22 21:17:39 +0200

    ✨ add tabula cask

---
+++ Casks/keyruu-tabula.rb
@@ -0,0 +1,16 @@
+cask "keyruu-tabula" do
+  version "1.1.0"
+  sha256 "ce99377e72ca23b0728aa296a57b505f07b5cabe7bb5787663dc15cc17ee5565"
+
+  url "https://github.com/keyruu/tabula/releases/download/#{version}/Tabula.zip",
+      verified: "github.com/keyruu/tabula/"
+  name "Tabula"
+  desc "Scroll with one finger and a modifier"
+  homepage "https://github.com/keyruu/tabula"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Tabula.app"
+
+  uninstall quit: "de.keyruu.Tabula"
+end
