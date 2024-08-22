e769f6403a67f78d83f24e925f43fc16dac40404
Lucas <keyruu@web.de>
2024-08-22 21:25:40 +0200

    💚 oh man

--- Casks/keyruu-tabula.rb
+++ Casks/keyruu-tabula.rb
@@ -2,7 +2,7 @@ cask "keyruu-tabula" do
   version "1.1.0"
   sha256 "ce99377e72ca23b0728aa296a57b505f07b5cabe7bb5787663dc15cc17ee5565"
 
-  url "https://github.com/keyruu/tabula/releases/download/#{version}/Tabula.zip",
+  url "https://github.com/keyruu/tabula/releases/download/#{version}/Tabula.zip"
   name "Tabula"
   desc "Scroll with one finger and a modifier"
   homepage "https://github.com/keyruu/tabula"
